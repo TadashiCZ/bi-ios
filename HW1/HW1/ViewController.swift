4070a8722f1561b6a8c76beaa672a665f98e4ac2
TadashiCZ <tadashicz@gmail.com>
2019-11-18 11:06:12 +0100

    add first homework - storyboard

---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Tadeáš on 15/11/2019.
+//  Copyright © 2019 Tadeáš. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
